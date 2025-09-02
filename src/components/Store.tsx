45a68c43b74bb384334e2ab3c317fd67208406f1
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-09-02 21:47:30 +0200

    Fix Ondina

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -157,13 +157,18 @@ const PublicStoreView: React.FC = () => {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 1440 320"
-                className="w-full h-20"
+                className="w-full h-24"
                 preserveAspectRatio="none"
               >
                 <path
                   fill="#1f2937" // grigio scuro sotto
                   fillOpacity="1"
-                  d="M0,96L80,112C160,128,320,160,480,186.7C640,213,800,235,960,213.3C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+                  d="M0,160 
+         C120,200,240,120,360,160 
+         C480,200,600,280,720,240 
+         C840,200,960,120,1080,160 
+         C1200,200,1320,280,1440,240 
+         L1440,320L0,320Z"
                 ></path>
               </svg>
             </div>

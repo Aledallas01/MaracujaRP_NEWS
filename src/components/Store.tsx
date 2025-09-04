b1a637797ebe8534a6343b0474611a0833506f2f
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-09-04 11:11:40 +0200

    Fix????????

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -166,7 +166,7 @@ const PublicStoreView: React.FC = () => {
           </button>
 
           {/* Logo responsive */}
-          <div className="relative z-30 flex justify-center mb-[-200px] sm:mb-12">
+          <div className="relative z-30 flex justify-center mb-12 sm:mb-16 lg:mb-20">
             <img
               src="/trasparent-logo.png"
               alt="Logo Store"
